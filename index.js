4219416bd6c7cd037c9466069dede3eeb0f02855
Dean Stigall <stigallrealestate@gmail.com>
2017-11-10 19:01:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code in this file!
 const currentUser = 'Killian';
-const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser = '!';
